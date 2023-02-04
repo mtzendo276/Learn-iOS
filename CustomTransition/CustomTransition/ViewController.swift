836b830f5452f28da173fd35a5d04ad45e6be75f
Victor Chen <victor.chen@mtzendo.com>
2023-02-04 20:56:51 +1300

    add custom transition

---
+++ CustomTransition/CustomTransition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomTransition
+//
+//  Created by Chen Yue on 4/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- TransparentBg/Test/SecondViewController.swift
+++ TransparentBg/Test/SecondViewController.swift
@@ -11,8 +11,6 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5)
     }
 
